53daa191a7713e1d68d826f68eca23c493fa7d9d
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-14 00:43:24 +0200

    Redirect '/all-courses' route to '/courses' route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css'
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, Redirect, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -44,6 +44,7 @@ class App extends Component {
             <Switch>
               <Route path="/users" component={Users} />
               <Route path="/courses" component={Courses} />
+              <Redirect from="/all-courses" to="/courses" />
               <Route render={() => <h1 style={{textAlign: 'center'}}>404 Not Found</h1>} />
             </Switch>
           </div>
